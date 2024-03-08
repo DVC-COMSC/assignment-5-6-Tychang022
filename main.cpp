5e8365e1bed0e5f99c995b63947887a6c6561913
Tychang022 <156855881+Tychang022@users.noreply.github.com>
2024-03-08 04:41:35 +0000

    TC

--- main.cpp
+++ main.cpp
@@ -1,13 +1,24 @@
 
 #include <iostream>
 using namespace std;
+int printRightBottomTriangleIndices(int rows, int columns) {
+    int totalCells = rows * columns;
+    int count = 0;
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < columns; ++j) {
+            if (j >= i) { 
+                cout << i << "," << j << endl;
+                ++count;
+            }
+        }
+    }
+    return count;
+}
 
 int main()
-{
-	int N;
-	cin >> N;
-
-	/***************************************************
-	 * Code Your program here
-	 ***************************************************/
+ {
+    int rows = 3;
+    int columns = 3;
+    printRightBottomTriangleIndices(rows, columns);
+    return 0;
 }
